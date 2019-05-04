27111ccb2168df769e5684d510d40842886c606c
Robson Junior <robsonjunior1994@gmail.com>
2019-05-03 21:44:59 -0300

    Criado a classe de modelo e a sua classe de serviço

---
+++ desafio/desafio/Models/Item.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace desafio.Models
+{
+    public class Item
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        //Type diz se é CD/DVD/LIVRO
+        public enum Type
+        {
+            none,
+            cd,
+            dvd,
+            book
+        }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("State")]
+        //State representa Disponivel ou emprestado.
+        public bool State { get; set; }
+
+        [BsonElement("Year")]
+        public string Year { get; set; }
+
+        [BsonElement("Genres")]
+        public string Genres { get; set; }
+
+        [BsonElement("Desciption")]
+        public string Description { get; set; }
+
+    }
+}
---
+++ desafio/desafio/Services/ItemService.cs
@@ -0,0 +1,53 @@
+﻿using desafio.Models;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace desafio.Services
+{
+    public class ItemService
+    {
+        private readonly IMongoCollection<Item> _items;
+
+        public ItemService(IConfiguration config)
+        {
+            var client = new MongoClient(config.GetConnectionString("DesafioDb"));
+            var database = client.GetDatabase("DesafioDb");
+            _items = database.GetCollection<Item>("Items");
+        }
+
+        public List<Item> Get()
+        {
+            return _items.Find(item => true).ToList();
+        }
+
+        public Item Get(string id)
+        {
+            return _items.Find<Item>(item => item.Id == id).FirstOrDefault();
+        }
+
+        public Item Create(Item item)
+        {
+            _items.InsertOne(item);
+            return item;
+        }
+
+        public void Update(string id, Item itemIn)
+        {
+            _items.ReplaceOne(item => item.Id == id, itemIn);
+        }
+
+        public void Remove(Item itemIn)
+        {
+            _items.DeleteOne(item => item.Id == itemIn.Id);
+        }
+
+        public void Remove(string id)
+        {
+            _items.DeleteOne(item => item.Id == id);
+        }
+    }
+}
