26b6aca37c36b88f687a7eb18340ab429cff7df8
Robson Junior <robsonjunior1994@gmail.com>
2019-05-06 23:30:53 -0300

    Fiz o listar, excluir e editar

--- desafio/InterfaceDesafio/wwwroot/js/site.js
+++ desafio/InterfaceDesafio/wwwroot/js/site.js
@@ -2,7 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
+const uri = "https://localhost:44393/api/item";
+let items = null;
 
 $(function () {
     $('#form').bind('submit', function (e) {
@@ -20,7 +21,7 @@ $(function () {
         console.log(item);
 
         $.ajax({
-            url: "https://localhost:44393/api/item",
+            url: uri,
             type: 'POST',
             contentType: "application/json",
             data: JSON.stringify(item),
@@ -35,3 +36,120 @@ $(function () {
         });
     });
 });
+
+/* --------------------------------------------------------------------------------------------- */
+$(document).ready(function () {
+    getData();
+});
+
+function getData() {
+    $.ajax({
+        type: "GET",
+        url: uri,
+        cache: false,
+        success: function (data) {
+            const tBody = $("#items");
+
+            $(tBody).empty();
+            
+            $.each(data, function (key, item) {
+                const tr = $("<tr></tr>")
+                    .append($("<td></td>").text(item.type))
+                    .append($("<td></td>").text(item.name))
+                    .append($("<td></td>").text(item.state))
+                    .append($("<td></td>").text(item.year))
+                    .append($("<td></td>").text(item.genres))
+                    .append($("<td></td>").text(item.description))
+                  
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Emprestar</button>").on("click", function () {
+                                emprestarItem(item.id);
+                            })
+                        )
+                    )
+                    .append(
+                        $("<td></td>").append(
+                            $("<button>Pegar</button>").on("click", function () {
+                                pegarItem(item.id);
+                            })
+                        )
+                ).append(
+                    $("<td></td>").append(
+                        $("<a href='#my-top'><button class='editar' >Editar</button></a>").on("click", function () {
+                            editItem(item.id);
+                        })
+                    )
+                ).append(
+                    $("<td></td>").append(
+                        $("<button class='btn-danger'>X</button>").on("click", function () {
+                            deleteItem(item.id);
+                        })
+                    )
+                );
+
+                tr.appendTo(tBody);
+            });
+
+            items = data;
+        }
+    });
+}
+
+function editItem(id) {
+    $.each(items, function (key, item) {
+        if (item.id === id) {
+            $("#edit-id").val(item.id);
+            $("#edit-type").val(item.type);
+            $("#edit-name").val(item.name);
+            $("#edit-state").val(item.state);
+            $("#edit-year").val(item.year);
+            $("#edit-genres").val(item.genres);
+            $("#edit-description").val(item.description);
+        }
+    });
+    $("#spoiler").css({ display: "block" });
+}
+
+$(".my-form").on("submit", function (e) {
+    e.preventDefault();
+
+    const item = {
+        id: $("#edit-id").val(),
+        type: $("#edit-type").val(),
+        name: $("#edit-name").val(),
+        state: $("#edit-state").val(),
+        year: $("#edit-year").val(),
+        genres: $("#edit-genres").val(),
+        description: $("#edit-description").val()
+    };
+    console.log(item);
+    $.ajax({
+        url: uri + "/" + $("#edit-id").val(),
+        type: "PUT",
+        accepts: "application/json",
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        success: function (result) {
+            getData();
+        }
+    });
+
+    closeInput();
+    return false;
+});
+
+function closeInput() {
+    $("#spoiler").css({ display: "none" });
+}
+
+
+function deleteItem(id) {
+    $.ajax({
+        url: uri + "/" + id,
+        type: "DELETE",
+        success: function (result) {
+            getData();
+        }
+    });
+}
